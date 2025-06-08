0001721f487b170e8b7f8c767ef7e8fdd1bf94c5
Hendreu <contatohendreu@gmail.com>
2025-06-08 19:09:04 -0300

    crawl single page with error handler

--- crawl.js
+++ crawl.js
@@ -1,5 +1,32 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`)
+
+    try{
+        const resp = await fetch(currentURL)
+
+        if(resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+            return
+
+        }
+
+        const contentType = resp.headers.get('content-type')
+        if (!contentType.includes('text/html')){
+
+            console.log(`non html response, content type: ${contentType} on page ${currentURL}`)
+            return    
+
+        }
+
+        console.log(await resp.text())
+
+    }catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML(htmlBody, baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -7,7 +34,9 @@ function getURLsFromHTML(htmlBody, baseURL){
     for (const linkElement of linkElements){
 
         if(linkElement.href.slice(0, 1) === '/'){
+
             //relative
+
             try{
             const urlObj = new URL (`${baseURL}${linkElement.href}`)
             urls.push(urlObj.href)
@@ -15,7 +44,9 @@ function getURLsFromHTML(htmlBody, baseURL){
                     console.log(`error with relative url: ${err.message}`)
                 }
         } else {
-                        //absolute
+
+            //absolute
+
             try {
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
@@ -42,5 +73,6 @@ function normalizeURL(urlString){
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage,
 }
