09730caa0bec16318d3ad18c5ba6fd5ee9d07513
Hendreu <contatohendreu@gmail.com>
2025-06-08 19:42:38 -0300

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,78 +1,93 @@
-const { JSDOM } = require('jsdom')
+const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL){
-    console.log(`actively crawling: ${currentURL}`)
+async function crawlPage(baseURL, currentURL, pages) {
+  baseURLObj = new URL(baseURL);
+  currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
 
-    try{
-        const resp = await fetch(currentURL)
+  const normalizeCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizeCurrentURL] > 0) {
+    pages[normalizeCurrentURL]++;
+    return pages;
+  }
 
-        if(resp.status > 399){
-            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
-            return
+  pages[normalizeCurrentURL] = 1;
 
-        }
+  console.log(`actively crawling: ${currentURL}`);
 
-        const contentType = resp.headers.get('content-type')
-        if (!contentType.includes('text/html')){
+  try {
+    const resp = await fetch(currentURL);
 
-            console.log(`non html response, content type: ${contentType} on page ${currentURL}`)
-            return    
-
-        }
-
-        console.log(await resp.text())
-
-    }catch (err) {
-        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    if (resp.status > 399) {
+      console.log(
+        `error in fetch with status code: ${resp.status} on page ${currentURL}`
+      );
+      return pages;
     }
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `non html response, content type: ${contentType} on page ${currentURL}`
+      );
+      return pages;
+    }
+
+    const htmlBody = await resp.text;
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+    for(const nextURL of nextURLs){
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+  } catch (err) {
+    console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+  }
+  return pages
 }
 
-function getURLsFromHTML(htmlBody, baseURL){
-    const urls = []
-    const dom = new JSDOM(htmlBody)
-    const linkElements = dom.window.document.querySelectorAll('a')
-    for (const linkElement of linkElements){
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative
 
-        if(linkElement.href.slice(0, 1) === '/'){
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`);
+      }
+    } else {
+      //absolute
 
-            //relative
-
-            try{
-            const urlObj = new URL (`${baseURL}${linkElement.href}`)
-            urls.push(urlObj.href)
-                } catch (err){
-                    console.log(`error with relative url: ${err.message}`)
-                }
-        } else {
-
-            //absolute
-
-            try {
-                const urlObj = new URL(linkElement.href)
-                urls.push(urlObj.href)
-            } catch (err) {
-                console.log(`error with the absolute url: ${err.message}`)
-            }
-
-
-        }
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with the absolute url: ${err.message}`);
+      }
     }
-    return urls
+  }
+  return urls;
 }
 
-function normalizeURL(urlString){
-    
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
 
-    if(hostPath.length > 0 && hostPath.slice(-1) === '/'){
-        return hostPath.slice(0, -1)
-    }
-    return hostPath
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = {
-    normalizeURL,
-    getURLsFromHTML,
-    crawlPage,
-}
+  normalizeURL,
+  getURLsFromHTML,
+  crawlPage,
+};
